fb84dedd8e7c57e04da8457c8d828bc49c9541ef
Kaja <kathleengracenoble@gmail.com>
2022-10-28 17:50:15 -0400

    finished add_entry fn

--- src/api.py
+++ src/api.py
@@ -1,25 +1,54 @@
 from flask import Flask, request
 from pydantic import ValidationError
-import api_fns as f
+import src.api_fns as f
 import json 
+import os
+import pdb 
+
+ENV = os.getenv('ENV')
 
 ## NEXT STEP: create end points and supporting fns for library upload fn
 
-app = Flask(__name__)
+def create_app(env):
+    app = Flask(__name__)
 
-@app.route('/home', methods=['GET'])
-def home():
-    return 'WELCOME TO CABINET'
 
-@app.route('/blob', methods=['GET', 'POST'])
-def blob_store():
-    if request.method == 'GET':
-        pass
-    if request.method == 'POST':
-        blob_bytes = request.get_json()['blob_bytes']
-        blob_id = f.post_blob(blob_bytes) 
-        return json.dumps({'blob_id':blob_id})
+    @app.route('/home', methods=['GET'])
+    def home():
+        return 'WELCOME TO CABINET'
 
-@app.route('/drawers', methods=['GET','POST'])
-def drawers():
-    pass 
+
+    @app.route('/blob', methods=['GET', 'POST'])
+    def store_blob():
+        if request.method == 'GET':
+            pass
+        if request.method == 'POST':
+            blob_bytes = request.get_json()['blob_bytes']
+            blob_id = f.add_blob(blob_bytes) 
+            return json.dumps({'blob_id':blob_id})
+
+
+    @app.route('/drawer', methods=['GET','POST'])
+    def drawer():
+        table_name = 'drawer'
+        feilds = ['entry_id','photo_id','channel','title', 'blob_id']
+        if request.method == 'GET':
+            pass
+        if request.method == 'POST': 
+            post_dict = request.get_json()
+            new = post_dict['new_blob']
+            if new:
+                entry_id = f.add_entry(table_name, post_dict['metadata'])
+                return json.dumps({'status_code':200, 'entry_id':entry_id})
+            else:
+                pass
+            
+    return app 
+
+
+if ENV != 'testing':
+    app = create_app(ENV)
+
+if __name__ == '__main__':
+    host = f.get_env_host(ENV)
+    app.run('localhost', 5050, debug=True)
--- src/api_fns.py
+++ src/api_fns.py
@@ -7,12 +7,19 @@ def get_conn_str(env:str='dev_local', config_file:str='config/config.yaml') ->st
     conn_str = config_dict[env]['conn_str']
     return conn_str
 
-def db_connect(env:str='dev_local', autocommit:bool=True):
+def db_connect(env:str='dev_local', autocommit:bool=True) ->tuple:
     conn_str = get_conn_str(env)
     conn = psycopg2.connect(conn_str)
     cur = conn.cursor()
     conn.autocommit = autocommit
     return conn, cur 
+
+def get_env_host(env:str='dev_local', config_file:str='config/config.yaml')->str:
+    with open(f'{config_file}', 'r') as f:
+        config_dict = yaml.safe_load(f)
+    env_host = config_dict[env]['host']
+    return env_host
+
 
 def add_blob(blob_bytes:str, env:str='dev_local'):
     try:
@@ -23,4 +30,35 @@ def add_blob(blob_bytes:str, env:str='dev_local'):
     finally:
         cur.close()
         conn.close()
-        return blob_id 
+        return blob_id
+
+
+def generate_sql_to_insert_metadata(table, metadata:dict):
+    # generate str of column names
+    columns:list = metadata.keys()
+    col_str = str
+    for i in columns:
+        col_str += i + ', '
+    l = len(col_str) 
+    col_str = col_str[0:-2]
+    # generate str of entry values
+    entry_vals = tuple(metadata.values())
+    s = ("%s,"*len(entry_vals))[0:-1]
+    # subs = str(tuple(entry_vals.values()))[1:-1]
+    return col_str, s, entry_vals
+
+
+def add_entry(table, metadata:dict, env:str='dev_local')->int:
+    col_str, s, entry_vals = generate_sql_to_insert_metadata(table, metadata)
+    try:
+        conn, cur = db_connect(env)
+        cur.execute(f'INSERT INTO {table}({col_str}) VALUES({s})', entry_vals)
+        cur.execute(f'SELECT MAX (entry_id) FROM {table}') 
+        entry_id = cur.fetchone()[0] 
+    finally:
+        cur.close()
+        conn.close()
+        return entry_id
+
+
+ 
