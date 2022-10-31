44172c994d10809792a15a8b90b1e2f1e790a8bd
Kaja <kathleengracenoble@gmail.com>
2022-10-31 14:40:54 -0400

    finished adding new and updated entries to tables

--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from pydantic import ValidationError
+from src.api_fns import db_connect
 import src.api_fns as f
 import json 
 import os
@@ -19,6 +20,7 @@ def create_app(env):
 
 
     @app.route('/blob', methods=['GET', 'POST'])
+    #TODO: learn about bytea postgres type. 
     def store_blob():
         if request.method == 'GET':
             pass
@@ -27,21 +29,32 @@ def create_app(env):
             blob_id = f.add_blob(blob_bytes) 
             return json.dumps({'blob_id':blob_id})
 
+    # NOTE: each data type has its own end point with predefined columns
+    # any changes to the table must be done in source code, not by user
 
     @app.route('/drawer', methods=['GET','POST'])
     def drawer():
-        table_name = 'drawer'
-        feilds = ['entry_id','photo_id','channel','title', 'blob_id']
-        if request.method == 'GET':
-            pass
-        if request.method == 'POST': 
-            post_dict = request.get_json()
-            new = post_dict['new_blob']
-            if new:
-                entry_id = f.add_entry(table_name, post_dict['metadata'])
-                return json.dumps({'status_code':200, 'entry_id':entry_id})
-            else:
+        try:
+            conn, cur = db_connect()
+            table_name = 'drawer'
+            feilds = ['entry_id','photo_id','channel','title', 'blob_id']
+            if request.method == 'GET':
                 pass
+            if request.method == 'POST': 
+                post_data = request.get_json()
+                new:bool = post_data['new_blob']
+                if new:
+                    entry_id = f.add_entry(table_name, post_data['metadata'])
+                    return json.dumps({'status_code':200, 'entry_id':entry_id})
+                else:
+                    updates:dict = post_data['update_dict']
+                    current_metadata:dict = f.get_current_metadata(table_name, entry_id)
+                    updated_metadata: dict = f.make_full_update_dict(updates, current_metadata)
+                    entry_id = f.add_entry(table_name, updated_metadata)
+                    return json.dumps({'status_code':200, 'entry_id': entry_id})
+        finally:
+            cur.close()
+            conn.close()
             
     return app 
 
--- src/api_fns.py
+++ src/api_fns.py
@@ -1,27 +1,33 @@
+from sqlite3 import sqlite_version
 import psycopg2
 import yaml
+import os
+import pdb
 
-def get_conn_str(env:str='dev_local', config_file:str='config/config.yaml') ->str:
+ENV = os.getenv('ENV')
+
+
+def get_conn_str(env:str=ENV, config_file:str='config/config.yaml') ->str:
     with open(f'{config_file}', 'r') as f:
         config_dict = yaml.safe_load(f)
     conn_str = config_dict[env]['conn_str']
     return conn_str
 
-def db_connect(env:str='dev_local', autocommit:bool=True) ->tuple:
+def db_connect(env:str=ENV, autocommit:bool=True) ->tuple:
     conn_str = get_conn_str(env)
     conn = psycopg2.connect(conn_str)
     cur = conn.cursor()
     conn.autocommit = autocommit
     return conn, cur 
 
-def get_env_host(env:str='dev_local', config_file:str='config/config.yaml')->str:
+def get_env_host(env:str=ENV, config_file:str='config/config.yaml')->str:
     with open(f'{config_file}', 'r') as f:
         config_dict = yaml.safe_load(f)
     env_host = config_dict[env]['host']
     return env_host
 
 
-def add_blob(blob_bytes:str, env:str='dev_local'):
+def add_blob(blob_bytes:str, env:str=ENV) -> int:
     try:
         conn, cur = db_connect(env)
         cur.execute('INSERT INTO blob (bytes) VALUES (%s)', (f'{blob_bytes}',))
@@ -33,32 +39,53 @@ def add_blob(blob_bytes:str, env:str='dev_local'):
         return blob_id
 
 
-def generate_sql_to_insert_metadata(table, metadata:dict):
+def build_insert_query(table:str, metadata:dict) -> tuple:
     # generate str of column names
     columns:list = metadata.keys()
-    col_str = str
+    col_str = ""
     for i in columns:
         col_str += i + ', '
-    l = len(col_str) 
     col_str = col_str[0:-2]
-    # generate str of entry values
+    # generate str of entry metadata values
     entry_vals = tuple(metadata.values())
+    # generate %s str of correct length
     s = ("%s,"*len(entry_vals))[0:-1]
-    # subs = str(tuple(entry_vals.values()))[1:-1]
-    return col_str, s, entry_vals
+    # create query
+    sql = f'INSERT INTO {table}({col_str}) VALUES({s})'
+    return sql, entry_vals
 
 
-def add_entry(table, metadata:dict, env:str='dev_local')->int:
-    col_str, s, entry_vals = generate_sql_to_insert_metadata(table, metadata)
-    try:
-        conn, cur = db_connect(env)
-        cur.execute(f'INSERT INTO {table}({col_str}) VALUES({s})', entry_vals)
-        cur.execute(f'SELECT MAX (entry_id) FROM {table}') 
-        entry_id = cur.fetchone()[0] 
-    finally:
-        cur.close()
-        conn.close()
-        return entry_id
+def add_entry(table:str, metadata:dict, cur, env:str=ENV)->int:
+    sql_query, entry_vals = build_insert_query(table, metadata)
+    cur.execute(sql_query, entry_vals)
+    cur.execute(f'SELECT MAX(entry_id) FROM {table}') 
+    entry_id = cur.fetchone()[0] 
+    return entry_id
 
+
+def get_column_names(table:str, cur, env:str=ENV) -> list: 
+    cur.execute(f'SELECT * FROM {table}')
+    col_names = [desc[0] for desc in cur.description]
+    return col_names
+    
+
+def get_current_metadata(table:str, entry_id:int, cur, env:str=ENV)-> dict:
+    s_sub = '%s'
+    cur.execute(f"SELECT * FROM {table} WHERE entry_id = {s_sub}", (entry_id,))
+    old_metadata_vals: list= list(cur.fetchone())
+    col_names:list = get_column_names(table, cur)
+    old_metadata = {}
+    for i in range(len(col_names)):
+        old_metadata[f'{col_names[i]}'] = old_metadata_vals[i]
+    return old_metadata
+
+
+def make_full_update_dict(updates:dict, old_metadata:dict):
+    full_update = updates 
+    for key in old_metadata.keys():
+        if not key in full_update:
+            full_update[key]=old_metadata[key]
+    full_update.pop('entry_id', 'No entry_id key') #or use del ?
+    return full_update
 
  
